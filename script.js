0e7f881b05bb08fe2e2ca1d65e1aff610026fc5e
stvnlee890 <stvnlee890@gmail.com>
2022-12-08 15:18:10 -0800

    finshed leetcode #9 Palindrome Number

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // ===== 26. REMOVE DUPLICATES FROM SORTED ARRAY =====
+
 // Do not allocate extra space for another array. You must do this by modifying
 // the input array in-place O(1) extra memory
 
 // TIME COMPLEXITY = O(N)
-    // Because N is dependent on the array length and we only traverse the array once
+// Because N is dependent on the array length and we only traverse the array once
 // SPACE COMPLEXIRY = O(N)
-    // We do not create an extra array, but we do create extra memory from these variables.
+// We do not create an extra array, but we do create extra memory from these variables.
 
 // my method
 // var removeDuplicates = function (nums) {
@@ -24,14 +25,36 @@
 // console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
 // console.log(removeDuplicates([1,2]));
 
-var removeDuplicates = function (nums) {
-    let insertIndex = 1
-    for (let i = 1; i < nums.length; i ++) {
-        if(nums[i - 1] !== nums[i]) {
-            nums[insertIndex] = nums[i]
-            insertIndex ++ 
-        }
+// using a for loop
+// var removeDuplicates = function (nums) {
+//     let insertIndex = 1
+//     for (let i = 1; i < nums.length; i ++) {
+//         if(nums[i - 1] !== nums[i]) {
+//             nums[insertIndex] = nums[i]
+//             insertIndex ++
+//         }
+//     }
+//     return insertIndex
+// }
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// ===== 9. PALINDROME NUMBER =====
+
+var isPalindrome = function (x) {
+  // Convert number to string
+  const str = x.toString();
+  // create left and right pointers
+  let left = 0;
+  let right = str.length -1;
+  while (left < right) {
+    if (str[left] === str[right]) {
+      left++;
+      right--;
+    } else {
+      return false;
     }
-    return insertIndex
-}
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+  }
+  return true
+};
+
+console.log(isPalindrome(10));
