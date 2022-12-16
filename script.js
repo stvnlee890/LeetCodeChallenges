89e2c2ea8c57594aa1120eb1ec953bc81a0f7c80
stvnlee890 <stvnlee890@gmail.com>
2022-12-16 13:31:25 -0800

    completed leetcode #27. Remove Element

--- script.js
+++ script.js
@@ -87,16 +87,66 @@
 // ===== 14. LONGEST COMMON PREFIX =====
 
 // SOLUTION CODE
-var longestCommonPrefix = function (strs) {
-  let prefix = "";
-  if(strs.length === 0) return prefix;
-  for (let i = 0; i < strs[0].length; i ++) {
-    const character = strs[0][i]
-    for(let j = 0; j < strs.length; j ++) {
-      if(strs[j][i] !== character) return prefix
+// var longestCommonPrefix = function (strs) {
+//   let prefix = "";
+//   if(strs.length === 0) return prefix;
+//   for (let i = 0; i < strs[0].length; i ++) {
+//     const character = strs[0][i]
+//     for(let j = 0; j < strs.length; j ++) {
+//       if(strs[j][i] !== character) return prefix
+//     }
+//     prefix += character
+//   }
+//   return prefix
+// };
+// console.log(longestCommonPrefix(["pajama", "puma", "potato"]));
+
+// ===== 20. Valid Parenthesis ====
+// use hash map
+// var isValid = function (s) {
+//   let obj = {
+//     "(": ")",
+//     "[": "]",
+//     "{": "}",
+//   };
+
+//   let arr = [];
+// // obj[keys] stores the value of the key, so when we move on to the next iteration
+// // we hit the else if statement. What the else if statement is doing here is "killing two birds with one stone"
+// // we're popping off the value in the array and checking that value with the current value in the index. so with the argument isValid("()[]{}")
+// // we start off with an empty array every other iteration.
+//   for (let i = 0; i < s.length; i++) {
+//     const keys = s[i];
+//     if (obj[keys]) {
+//       arr.push(obj[keys]);
+//       console.log(arr)
+//       // arr.pop directly mutates the array.
+//       // So everytime this statement is false, we're always restarting with a new array.
+//       // "({)}" this would come out as false and hit the else statement
+//     } else if (keys !== arr.pop()) {
+//       return false;
+//     }
+//   }
+//   console.log(arr)
+//   return !arr.length;
+// };
+
+// console.log(isValid("()[]{}"));
+
+// ===== 27. Remove Element ======
+var removeElement = function (nums, val) {
+  let start = 0;
+  while (start < nums.length) {
+    if (nums[start] === val) {
+      nums.splice(start, 1)
+      // we have to decrement start because we're modifying the original array in-place. 
+      // so after a new loop, we might "skip" over a value and not splice it. 
+      start--;
+    } else {
+      start++;
     }
-    prefix += character
   }
-  return prefix
+  return nums.length
 };
-console.log(longestCommonPrefix(["pajama", "puma", "potato"]));
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2));
